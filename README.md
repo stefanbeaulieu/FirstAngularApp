c4bd6884dc495cba9568742a2ccb2a4689616577
Stefan Beaulieu <beaulieustefan@gmail.com>
2017-07-16 23:29:46 -0400

    setting up initial files for tutorial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FirstAngularApp
+
+1. Creating my first Angular app via TeamTreeHouse.com instructional video
---
+++ photo_blog/wwwroot/app.88ec8f00f028b81d9ffd.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],[function(t,e,c){"use strict";c(1)},function(t,e){t.exports="// removed by extract-text-webpack-plugin"}]);
+//# sourceMappingURL=app.88ec8f00f028b81d9ffd.js.map
